bc904088ed2251ba2eec4173a3e061b40a474361
bracesproul <braceasproul@gmail.com>
2024-11-26 20:27:20 -0800

    feat: Evals

---
+++ src/evals/twitter/index.ts
@@ -0,0 +1,36 @@
+import { type Example, Run } from "langsmith";
+import { evaluate, EvaluationResult } from "langsmith/evaluation";
+import "dotenv/config";
+import { generatePostGraph } from "../../agent/subgraphs/generate-post/graph.js";
+
+const runGraph = async (
+  input: Record<string, any>
+): Promise<Record<string, any>> => {
+  return await generatePostGraph.invoke(input);
+};
+
+const evaluatePost = (run: Run, example?: Example): EvaluationResult => {
+  if (!example) {
+    throw new Error("No example provided");
+  }
+  if (!example.outputs) {
+    throw new Error("No example outputs provided");
+  }
+  if (!run.outputs) {
+    throw new Error("No run outputs provided");
+  }
+
+  // TODO: Implement evaluation logic
+  throw new Error("Evaluation logic not implemented");
+};
+
+async function runEval() {
+  const datasetName = "sma:generate-post:twitter";
+  await evaluate(runGraph, {
+    data: datasetName,
+    evaluators: [evaluatePost],
+    experimentPrefix: "Post Generation - Twitter",
+  });
+}
+
+runEval();
