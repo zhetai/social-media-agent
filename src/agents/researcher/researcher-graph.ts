ed07b8778d489633e39c4eb19c247c6a4e000f24
bracesproul <braceasproul@gmail.com>
2024-12-04 13:29:18 -0800

    fmt

--- src/agents/researcher/researcher-graph.ts
+++ src/agents/researcher/researcher-graph.ts
@@ -3,24 +3,24 @@ import { ResearcherGraphAnnotation } from "./researcher-state.js";
 
 /**
  * Given either a topic, or array of links, preform research and generate a report.
- * 
+ *
  * Inputs:
  * - Research report request
  * - Array of links (optional)
- * 
+ *
  * If given an array of link:
  * Use the existing `verify-links` subgraph to get page contents for each link. Then,
  * generate a report on each page content. Finally, combine all reports into a single parent report.
  * Once the parent report is generated, use a reflection style prompt to ensure the report is relevant to the topic.
  * If not, search for new context.
- * 
+ *
  * If not given an array of links:
  * - Search for links about the topic:
  *   - Exa API/Tavily API
  *   - Arcade `SearchRecentTweetsByKeywords`
  *   - Smol AI: https://buttondown.com/ainews
- * 
- * 
+ *
+ *
  * Example:
  * - user collects links to tweets, blogs, and reddit posts about OpenAI's new product.
  * - report agent generates reports on each of these links.
@@ -29,19 +29,19 @@ import { ResearcherGraphAnnotation } from "./researcher-state.js";
 
 const researcherWorkflow = new StateGraph(ResearcherGraphAnnotation)
   .addNode("generateSearchPlan", () => {
-    throw new Error("Not implemented")
+    throw new Error("Not implemented");
   })
   .addNode("collectLinks", () => {
-    throw new Error("Not implemented")
+    throw new Error("Not implemented");
   })
   .addNode("generateReport", () => {
-    throw new Error("Not implemented")
+    throw new Error("Not implemented");
   })
   .addNode("combineReports", () => {
-    throw new Error("Not implemented")
+    throw new Error("Not implemented");
   })
   .addNode("generatePost", () => {
-    throw new Error("Not implemented")
+    throw new Error("Not implemented");
   })
   .addEdge(START, "generateSearchPlan")
   .addEdge("generateSearchPlan", "collectLinks")
